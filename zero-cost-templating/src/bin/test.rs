d296e5ab37803db70f8e856dd5281106fd6cfa26
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-10 17:36:43 +0100

    Jo

--- zero-cost-templating/src/bin/test.rs
+++ zero-cost-templating/src/bin/test.rs
@@ -1,31 +1,16 @@
-#![feature(coroutines)]
-extern crate alloc;
-#[must_use]
 pub struct PartialBlockPartialTemplate0<PartialType, EndType> {
     partial_type: PartialType,
     end_type: EndType,
 }
-#[must_use]
 pub struct PartialBlockPartialTemplate2<PartialType, EndType> {
     partial_type: PartialType,
     end_type: EndType,
-}
-impl<PartialType, EndType> PartialBlockPartialTemplate2<PartialType, EndType> {
-    pub fn map_inner<NewPartialType, NewEndType>(
-        self,
-        new_partial_type: NewPartialType,
-        new_end_type: NewEndType,
-    ) -> PartialBlockPartialTemplate2<NewPartialType, NewEndType> {
-        PartialBlockPartialTemplate2 {
-            partial_type: new_partial_type,
-            end_type: new_end_type,
-        }
-    }
 }
 // every type could probably have a non-generic companion struct and them that is an associate type with the actual struct?
 pub trait Templaty<Partial, End> {
 
 }
+// we should try this generically because then templates could be used cross-crate
 impl<PartialTypePartial, PartialTypeEnd, PartialType: Templaty<PartialTypePartial, PartialTypeEnd>, EndType> PartialBlockPartialTemplate0<PartialType, EndType> {
     pub fn partial_block_partial_template0(
         template: Self,
