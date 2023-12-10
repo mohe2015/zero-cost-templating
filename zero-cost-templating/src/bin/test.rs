f9fd779ca3ec74e84111e79f3416972b1f5f42c7
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-10 17:45:25 +0100

    what about this

--- zero-cost-templating/src/bin/test.rs
+++ zero-cost-templating/src/bin/test.rs
@@ -1,20 +1,18 @@
-pub struct PartialBlockPartialTemplate0<PartialType, EndType> {
-    partial_type: PartialType,
-    end_type: EndType,
+pub struct Template<Type, Partial, End> {
+    t: Type,
+    partial: Partial,
+    end: End,
 }
-pub struct PartialBlockPartialTemplate2<PartialType, EndType> {
-    partial_type: PartialType,
-    end_type: EndType,
-}
-// every type could probably have a non-generic companion struct and them that is an associate type with the actual struct?
-pub trait Templaty<Partial, End> {
 
-}
+pub struct PartialBlockPartialTemplate0;
+pub struct PartialBlockPartialTemplate2;
 // we should try this generically because then templates could be used cross-crate
-impl<PartialTypePartial, PartialTypeEnd, PartialType: Templaty<PartialTypePartial, PartialTypeEnd>, EndType> PartialBlockPartialTemplate0<PartialType, EndType> {
+impl<PartialType, PartialPartial, PartialEnd, End>
+    Template<PartialBlockPartialTemplate0, Template<PartialType, PartialPartial, PartialEnd>, End>
+{
     pub fn partial_block_partial_template0(
         template: Self,
-    ) -> PartialType<(), PartialBlockPartialTemplate2<(), EndType>> {
+    ) -> Template<PartialType, (), Template<PartialBlockPartialTemplate2, (), End>> {
         todo!()
     }
 }
