838bb807c22789cf82556629a5c439e15c545843
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-10 17:48:13 +0100

    Idea

--- zero-cost-templating/src/bin/test.rs
+++ zero-cost-templating/src/bin/test.rs
@@ -1,18 +1,22 @@
-pub struct Template<Type, Partial, End> {
+pub struct Template<Type, Partial, After> {
     t: Type,
     partial: Partial,
-    end: End,
+    after: After,
 }
 
 pub struct PartialBlockPartialTemplate0;
 pub struct PartialBlockPartialTemplate2;
 // we should try this generically because then templates could be used cross-crate
-impl<PartialType, PartialPartial, PartialEnd, End>
-    Template<PartialBlockPartialTemplate0, Template<PartialType, PartialPartial, PartialEnd>, End>
+impl<PartialType, PartialPartial, PartialAfter, After>
+    Template<
+        PartialBlockPartialTemplate0,
+        Template<PartialType, PartialPartial, PartialAfter>,
+        After,
+    >
 {
     pub fn partial_block_partial_template0(
         template: Self,
-    ) -> Template<PartialType, (), Template<PartialBlockPartialTemplate2, (), End>> {
+    ) -> Template<PartialType, (), Template<PartialBlockPartialTemplate2, (), After>> {
         todo!()
     }
 }
