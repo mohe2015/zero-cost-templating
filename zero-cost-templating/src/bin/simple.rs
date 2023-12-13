1f9c11bfc41d54de878cddd1264e963596f867b0
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-14 00:02:05 +0100

    Fix

--- zero-cost-templating/src/bin/simple.rs
+++ zero-cost-templating/src/bin/simple.rs
@@ -12,10 +12,10 @@ use zero_cost_templating::template_stream;
 
 // export RUSTFLAGS="-Z proc-macro-backtrace"
 // cargo build
-// cargo expand --package zero-cost-templating --bin idea5
-// cargo run --release --bin idea5
+// cargo expand --package zero-cost-templating --bin simple
+// cargo run --release --bin simple
 
-// RUSTFLAGS="-Zprint-type-sizes" cargo run --release --bin idea5 > type-sizes.txt
+// RUSTFLAGS="-Zprint-type-sizes" cargo run --release --bin simple > type-sizes.txt
 // search for
 // `{static coroutine@
 
