04b664df5c1c5b1a8f04b8ffea29574cb5946d3a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-14 23:57:14 +0100

    test

--- zero-cost-templating/tests/ui/compile_fail_expand/wrong_type.rs
+++ zero-cost-templating/tests/ui/compile_fail_expand/wrong_type.rs
@@ -1,1 +1,22 @@
-../compile_fail_no_expand/wrong_type.rs
+#![deny(warnings)]
+#![feature(coroutines)]
+
+extern crate alloc;
+
+use alloc::borrow::Cow;
+
+use zero_cost_templating_macros::template_stream;
+
+#[template_stream("test.html.hbs")]
+pub async fn test() {
+    let template = test_initial0();
+    let template = template.test_template0();
+    let page_title = Cow::from("thetitle");
+    let template = template.test_page_title1(page_title);
+    let csrf_token = Cow::from("thetoken");
+    let template = template.test_csrf_token2(csrf_token);
+    let template = template.test_template6();
+    let test: String = template.test_copyright_year7(Cow::from("2023".to_string()));
+}
+
+pub fn main() {}
