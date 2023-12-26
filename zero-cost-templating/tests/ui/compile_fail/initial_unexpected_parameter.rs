a47f8d258a3ce952f5e60d8e128c29dbe86afc73
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-26 01:34:26 +0100

    wip

--- zero-cost-templating/tests/ui/compile_fail/initial_unexpected_parameter.rs
+++ zero-cost-templating/tests/ui/compile_fail/initial_unexpected_parameter.rs
@@ -9,17 +9,7 @@ use zero_cost_templating_macros::template_stream;
 
 #[template_stream("test.html.hbs")]
 pub async gen fn test() -> Cow<'static, str> {
-    let template = yields!(test_initial0("unexpected"));
-    let template = yields!(template.test_template0());
-    let page_title = Cow::from("thetitle");
-    let template = yields!(template.test_page_title1(page_title));
-    let template = yields!(template.test_template2());
-    let csrf_token = Cow::from("thetoken");
-    let template = yields!(template.test_csrf_token3(csrf_token));
-    let template = yields!(template.test_template4());
-    let template = yields!(template.test_template10());
-    let template = yields!(template.test_copyright_year11(Cow::from("2023".to_string())));
-    yields!(template.test_template12());
+    let template = yields!(test0("unexpected"));
 }
 
 pub fn main() {}
